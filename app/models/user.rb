aa640e097f830185a705f6b51a0f8fde044674db
kawakami-masachika <supercell1023koeda@gmail.com>
2019-03-13 20:14:50 +0900

    コールバックの処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  #validates :name, presence: true , length{maximum 200}
-  #validates :age, presence: true , numericality: true  
+  attr_accessor :fav_brand_1 , :fav_brand_2 , :fav_brand_3 , :fav_brand_4 , :fav_brand_5
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  
+  before_save :join_fav_brands
+
+  def join_fav_brands
+  #binding.pry
+    # self → いま操作しているユーザー情報
+    # fav_brand_1~5 → ブランド名(ナイキ, アディダス)
+    
+    self.fav_brand =  [self.fav_brand_1, self.fav_brand_2, self.fav_brand_3, self.fav_brand_4, self.fav_brand_5 ]
+    #self.fav_brand = self.fav_brand_1 + ',' + self.fav_brand_2 + ',' + self.fav_brand_3 + ',' + self.fav_brand_4 + ',' + self.fav_brand_5
+  end
 end
