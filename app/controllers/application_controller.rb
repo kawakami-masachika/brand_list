e292ab4c4ac83193fe2a49395f210fdf3d5e9652
kawakami-masachika <supercell1023koeda@gmail.com>
2019-03-13 20:12:34 +0900

    fav_brand 1~5 ストロングパラメータに追加、サインアウト後のパス設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,17 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
+
+   def after_sign_out_path_for(resource)
+    root_path
+  end
+  
   
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :name, :age, :sex, :email, :fav_brand, :password, :password_confirmation　]
+    added_attrs = [ :name, :age, :sex, :email, :fav_brand, :fav_brand_1, :fav_brand_2, :fav_brand_3 , :fav_brand_4, :fav_brand_5,
+                    :password, :password_confirmation]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
