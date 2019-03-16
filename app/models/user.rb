d3d0dd1d68105d0762f0b9f5ed4eca06fbcbc4ab
kawakami-masachika <supercell1023koeda@gmail.com>
2019-03-16 19:25:25 +0900

    ストロングパラメータ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ApplicationRecord
-  attr_accessor :fav_brand_1 , :fav_brand_2 , :fav_brand_3 , :fav_brand_4 , :fav_brand_5
+  attr_accessor :fav_brand_1 , :fav_brand_2 , :fav_brand_3
+
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   
-  before_save :join_fav_brands
-
+  before_save :join_fav_brands , unless: :created_at
+  
+  private
   def join_fav_brands
   #binding.pry
     # self → いま操作しているユーザー情報
     # fav_brand_1~5 → ブランド名(ナイキ, アディダス)
-    
-    self.fav_brand =  [self.fav_brand_1, self.fav_brand_2, self.fav_brand_3, self.fav_brand_4, self.fav_brand_5 ]
-    #self.fav_brand = self.fav_brand_1 + ',' + self.fav_brand_2 + ',' + self.fav_brand_3 + ',' + self.fav_brand_4 + ',' + self.fav_brand_5
+    #self.fav_brand =  [self.fav_brand_1, self.fav_brand_2, self.fav_brand_3, self.fav_brand_4, self.fav_brand_5 ]
+    self.fav_brand = self.fav_brand_1 + ' ' + self.fav_brand_2 + ' ' + self.fav_brand_3
   end
 end
