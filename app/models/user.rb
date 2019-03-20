768e3e07cb06f10adef3d7054467a737feb9a8c6
kawakami-masachika <supercell1023koeda@gmail.com>
2019-03-20 19:23:21 +0900

    コールバックの処理を値によって変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,24 @@
 class User < ApplicationRecord
   attr_accessor :fav_brand_1 , :fav_brand_2 , :fav_brand_3
-
+  
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   
-  before_save :join_fav_brands , unless: :created_at
+  before_save :join_fav_brands
+  #binding.pry
+  #before_update :join_fav_brands
+  
   
   private
-  def join_fav_brands
-  #binding.pry
+  def join_fav_brands 
     # self → いま操作しているユーザー情報
-    # fav_brand_1~5 → ブランド名(ナイキ, アディダス)
-    #self.fav_brand =  [self.fav_brand_1, self.fav_brand_2, self.fav_brand_3, self.fav_brand_4, self.fav_brand_5 ]
-    self.fav_brand = self.fav_brand_1 + ' ' + self.fav_brand_2 + ' ' + self.fav_brand_3
+    # fav_brand_1~5 → ブランド名(ナイキ, アディダス) 
+    if @fav_brand_1.blank? && @fav_brand_2.blank? && @fav_brand_3.blank?
+      fav_brand
+    else
+      #self.fav_brand =  [@fav_brand_1 , @fav_brand_2, @fav_brand_3].join("")
+      self.fav_brand = @fav_brand_1 + ' ' + @fav_brand_2 + ' ' + @fav_brand_3
+    end
   end
+
 end
