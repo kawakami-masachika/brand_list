dbafdf7a0e7d8e92561575f4c6c9f15cc6279876
kawakami-masachika <supercell1023koeda@gmail.com>
2019-04-10 23:38:00 +0900

    shopモデル,deviseのルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: {
+      confirmations: 'user/confirmations',
+      #omniauth_callbacks 'user/omniauth_callbacks',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations',
+      sessions: 'users/sessions', 
+      unlocks: 'user/unlocks',
+    }, only: [:confirmation,:password,:registration, :session, :unlock]
+
   root to:'users#index'
   resources :users, only: [:show,:index,:edit,:update]
+  resources :shops
 end
