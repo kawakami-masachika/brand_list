a9592d04b750c9b5ca02d44626763942e3020013
kawakami-masachika <supercell1023koeda@gmail.com>
2019-04-19 21:08:29 +0900

    createアクションの処理に追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,26 +10,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   #POST /resource
-    def create
-    build_resource(sign_up_params)
-    resource.join_fav_brands(params[:user][:fav_brand_1],params[:user][:fav_brand_2],params[:user][:fav_brand_3])
-    if resource.save
-      binding.pry
-      yield resource if block_given?
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
-        sign_up(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-      
-    else
-      clean_up_passwords resource
-      respond_with resource
-    end
+  def create
+    super
+    resource.join_fav_brands( params[:user][:fav_brand_1],
+                              params[:user][:fav_brand_2],
+                              params[:user][:fav_brand_3]
+                            )
   end
   
   # GET /resource/edit
