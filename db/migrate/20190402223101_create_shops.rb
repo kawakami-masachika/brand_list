ea871cfbfc0a8cf1b86f4e475b5b8e88536202f6
kawakami-masachika <supercell1023koeda@gmail.com>
2019-04-26 20:43:24 +0900

    shopモデル マイグレーションファイルの修正

--- db/migrate/20190402223101_create_shops.rb
+++ db/migrate/20190402223101_create_shops.rb
@@ -1,13 +1,13 @@
 class CreateShops < ActiveRecord::Migration[5.2]
   def change
     create_table :shops do |t|
-      t.string :name
+      t.string :shop_name
       t.string :address
       t.string :business_period
       t.string :staition
       t.string :tel
-      t.string :url
-      t.string :images
+      t.string :shop_url
+      t.string :shop_image
       t.text :shop_infomation
       t.integer :user_id
 
