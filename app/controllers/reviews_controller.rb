0d7735123a2c07b9251d4b56595a237fb4e330c2
Kiseki Shoji <o9o3.kiseki@gmail.com>
2019-05-18 15:04:19 +0900

    controller, modelのリファクタ

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,70 +1,59 @@
 class ReviewsController < ApplicationController
-  #before_action :set_review ,only:[:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index, :update]
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   def index
     @shop = Shop.find(params[:shop_id])
     @reviews = Review.page(params[:page]).per(10)
   end
 
-  def new 
+  def new
     @shop = Shop.find(params[:shop_id])
     @review = Review.new
-    #@review.user_id = current_user.id
-    #binding.pry
   end
 
   def create
     @shop = Shop.find(params[:shop_id])
     @review = Review.new(review_params)
     if @review.save
-      
-    # @review = @shop.reviews.new
-    # @review.attributes = review_params
-    # @review.user_id = current_user.id
-    # @review.save!
       redirect_to current_user
-    else 
+    else
       render :new
     end
   end
-  
+
   def edit
     @shop = Shop.find(params[:shop_id])
-    @review = Review.find(params[:id])
-    
   end
 
   def update
-    @shop = Shop.find(params[:shop_id])
-    @review = Review.find(params[:id])
-   if @review.update(review_params)
-      redirect_to @shop
-      flash[:norice] = "レビューを編集しました"
+    @shop = @review.shop
+    if @review.update(review_params)
+      redirect_to @shop, flash[:notice] =  "レビューを編集しました"
     else
-      render :edit
-      flash[:alert] = "レビューの編集に失敗しました"
+      render :edit, flash[:alert] = "レビューの編集に失敗しました"
     end
   end
 
   def show
     @shop = Shop.find(params[:shop_id])
-    @review = Review.find(params[:id])
   end
 
   def destroy
-    @shop = Shop.find(params[:shop_id])
-    @review = Review.find(params[:id])
+    @shop = @review.shop
     @review.destroy
     redirect_to @shop
   end
 
-  private 
+  private
+
   def review_params
-    params.require(:review).permit(:title,:comment).merge(shop_id: params[:shop_id],user_id: current_user.id)
+    # 書き方変更したほうがいいかも
+    params.require(:review).permit(:title, :comment).merge(shop_id: params[:shop_id], user_id: current_user.id)
   end
 
   def set_review
-    
+    @review = Review.find(params[:id])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,45 @@
-
 class UsersController < ApplicationController
-  
-  before_action :set_user , only:[:show,:edit,:update,:shops,:reviews,:option]
+  before_action :set_user, only: [:show, :edit, :update, :shops, :reviews, :option]
 
   def show
     @shops = @user.shops
     @reviews = @user.reviews
-  end 
+  end
 
   def edit
-    if @user.fav_brand?
-      @fav_brands = @user.fav_brand.split(' ')
-    else
-      @fav_brands = []
-    end
+    @fav_brands = if @user.fav_brand?
+                    @user.fav_brand.split(' ')
+                  else
+                    []
+                  end
   end
 
   def update
-    binding.pry
-    @user.join_fav_brands(params[:user][:fav_brand_1],params[:user][:fav_brand_2],params[:user][:fav_brand_3])
+    @user.join_fav_brands(params[:user][:fav_brand_1], params[:user][:fav_brand_2], params[:user][:fav_brand_3])
     if @user.update(user_params)
-      redirect_to @user 
-      flash[:notice] = 'ユーザー情報の編集をしました'
+      redirect_to @user, flash[:notice] = 'ユーザー情報の編集をしました'
     else
-      flash.now[:alert] = 'ユーザー情報を編集できませんでした'
-      render :edit
+      flash.now[:alert] = 'ユーザー情報を編集できませんでした', render :edit
     end
   end
 
   def shops
-    @shops = Shop.where(user_id: @user.id).page(params[:page]).per(8)
+    # @shops = Shop.where(user_id: @user.id).page(params[:page]).per(8)
+    @shops = @user.shops.page(params[:page]).per(8)
   end
-  
+
   def reviews
-    @reviews = Review.where(user_id: current_user.id).page(params[:page]).per(10)
+    # @reviews = Review.where(user_id: current_user.id).page(params[:page]).per(10)
+    @reviews = @user.reviews.page(params[:page]).per(10)
   end
 
   def option
-    unless @user == current_user
-      redirect_to root_path
-    end
+    redirect_to root_path unless @user == current_user
   end
 
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
@@ -51,6 +47,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :age, :sex, :fav_brand_1, :fav_brand_2, :fav_brand_3)
   end
-  
+
 
 end
